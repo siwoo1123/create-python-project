fe426201b14a60bc6e1122cb1b82fc956d18e96f
siwoo1123 <61868624+siwoo1123@users.noreply.github.com>
2021-05-17 23:00:53 +0900

    commit

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,12 @@
-console.log();
+var exec = require('child_process').exec,
+    child;
+
+console.log("Hello, World");
+
+child = exec("pwd", function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    }
+});
